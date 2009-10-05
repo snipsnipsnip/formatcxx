a00abcceb24d3392b96c81593b1f4d3e6dff131b
snip <s.skk.skk@gmail.com>
2009-10-05 20:08:38 +0900

    add HogeLex

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,13 @@
 #include "opencxx/parser/Ptree.h"
 #include "opencxx/parser/Program.h"
 #include "opencxx/parser/CerrErrorLog.h"
+#include "opencxx/parser/Token.h"
 
 using Opencxx::Parser;
-using Opencxx::Lex;
+using Opencxx::ILex;
 using Opencxx::Ptree;
 using Opencxx::CerrErrorLog;
+using Opencxx::Token;
 
 class FooProgram : public Opencxx::Program
 {
@@ -18,15 +20,61 @@ public:
         Program("foo")
     {
         index = 0;
-        buf = "int foo() { ";
+        buf = "int foo() /* hoge */ { ";
         size = std::strlen(buf) + 1;
+    }
+};
+
+class HogeLex : public Opencxx::ILex
+{
+    Ptree *comments;
+    
+public:
+    virtual int GetToken(Token &token_out)
+    {
+    }
+
+    virtual int LookAhead(int offset)
+    {
+    }
+
+    virtual int LookAhead(int offset, Token &token_out)
+    {
+    }
+
+    virtual char* Save()
+    {
+    }
+
+    virtual void Restore(char *pos)
+    {
+    }
+
+    virtual void GetOnlyClosingBracket(Token &token_out)
+    {
+    }
+
+    virtual Ptree* GetComments()
+    {
+        Ptree *c = comments;
+        comments = 0;
+        return c;
+    }
+
+    virtual Ptree* GetComments2()
+    {
+        return comments;
+    }
+
+    virtual unsigned LineNumber(char*, char*&, int&)
+    {
     }
 };
 
 int main()
 {
     FooProgram program;
-    Lex lex(&program);
+    HogeLex lex(&program);
     CerrErrorLog log;
     Parser parser(&lex, log);
     
